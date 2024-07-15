e9193dbd6782cbcae01d15ae7df1a748ecc01ff4
zygomaticontheway <mrzygomatic@gmail.com>
2024-07-15 23:20:32 +0200

    lesson 09 + hw  finish

--- my-app/src/homeworks/hw09/RandomFox.tsx
+++ my-app/src/homeworks/hw09/RandomFox.tsx
@@ -1,7 +1,8 @@
 
 import { useEffect, useState } from "react";
-import styles from './randomFox.modules';
+import styles from './randomFox.module.css'
 import MyButton from "../../components/myButton/MyButton";
+import { error } from "console";
 
 // !Задание
 // Повторите хук useEffect() Создайте компонент для отображения данных 
@@ -16,49 +17,60 @@ function RandomFox () {
     }
 
     useEffect(() => {
-        fetch ('https://randomfox.ca/floof/')
-            .then(res => res.json())
-            .then(data => console.log(data));
-
+        fetchFox();
     }, [])
 
     const fetchFox = () => {
         fetch('https://randomfox.ca/floof/')
             .then(res => res.json())
             .then(data => setFox(data))
+            .catch(error => console.error('Error fetching fox: ', error))
     }
     
     const [fox, setFox] = useState<IFoxData>({
         image: '',
         link: ''
     })
+    const [count, setCount] = useState(0)
+
+    const handlePlus = () => {
+        setCount(prev => prev + 1)
+    }
 
     const leftOrRight = () => {
         const regex = /(\d+)/;
         const match = fox.link.match(regex);
+        console.log(match);
+        
 
         if (match) {
             const number = parseInt(match[1], 10);
-    
+
             if (number % 2 === 0) {
-                <div className={styles.foxContainer}>
-                    <div>`${number} is even. Tricky fox is on the right. ----&gt`</div>
+                return(
+                <div className={styles.foxContainer} >
+                    <div className={styles.left}>{`${number} is even. Tricky fox is on the right. ---->`}</div>
                     <div className={styles.right}>
-                        <img src={fox.image} alt="tricky fox" />
+                        <img className={styles.img} src={fox.image} alt="tricky fox" />
                     </div>
                 </div>
+                )
             } else {
+                return(
                 <div className={styles.foxContainer}>
                     <div className={styles.left}>
-                        <img src={fox.image} alt="tricky fox" />
+                        <img className={styles.img} src={fox.image} alt="tricky fox" />
                     </div>
-                    <div className={styles.right}>`&lt---- ${number} is odd. Tricky fox is on the left.`</div>
+                    <div className={styles.right}>{`<---- ${number} is odd. Tricky fox is on the left.`}</div>
                     
                 </div>
+                )
                 
             }
         } else {
-            <div className={styles.foxContainer}>Tricky fox is missing</div>
+            return(
+                <div className={styles.foxContainer}>Tricky fox is missing</div>
+            )
         }    
     }
 
@@ -71,8 +83,16 @@ function RandomFox () {
                 <li>выведите случайную картинку из api https://randomfox.ca/</li>
                 <li>код должен сработать один раз при монтировании компонента</li>
             </ul>
-            leftOrRight()
-            <MyButton type="button" onClick={leftOrRight}/>
+            <p> это висит для проверки 👇 условия задачи</p>
+            <p>{count}</p>
+            <MyButton onClick={handlePlus} name={'plus'}/>
+            <h3>🦊 Бонус 🦊</h3>
+            <ul>
+                <li>В зависимости от четное или нечетное число в имени файла</li>
+                <li>лиса будет слева или справа</li>
+            </ul>
+            {leftOrRight()}
+            <MyButton type="button" onClick={fetchFox} name="Fetch fox"/>
         </div>
     )
 }
